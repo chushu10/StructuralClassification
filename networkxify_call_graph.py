5ad1d863545d080f35742b5d6a6762a0a7f7fc6c
Chushu <liuchushu10@gmail.com>
2017-03-10 20:05:25 +0800

    networkxify function call graph

--- networkxify_call_graph.py
+++ networkxify_call_graph.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import numpy as np
 import common.pz as pz
 import networkx as nx
 import os, argparse
+from generate_call_graph import generate
 from common.utils import use_progressbar, count_file
 
 def networkxify(cg):
@@ -14,26 +16,35 @@ def networkxify(cg):
     # nx graph for FCG extracted from APK: nodes = method_name,
     # labels = encoded instructions
     fcg = nx.DiGraph()
-    methods = self.d.get_methods()
-    for method in methods:
-        node_name = self.get_method_label(method)
+    for method in cg:
+        node_name = get_method_label(method)
 
         # find calls from this method
         children = []
-        for cob in method.XREFto.items:
-            remote_method = cob[0]
-            children.append(self.get_method_label(remote_method))
+        for cob in cg[method]['out_neighbors']:
+            children.append(get_method_label(cob))
 
-        # find all instructions in method and encode using coloring
-        instructions = []
-        for i in method.get_instructions():
-            instructions.append(i.get_name())
-        encoded_label = self.color_instructions(instructions)
+        # use the pre-generated labels
+        label_str = cg[method]['label']
+        label_len = len(label_str)
+        h = [0] * label_len
+        for i in range(label_len):
+            h[i] = int(label_str[i])
+        encoded_label = np.array(h)
+
         # add node, children and label to nx graph
         fcg.add_node(node_name, label=encoded_label)
         fcg.add_edges_from([(node_name, child) for child in children])
 
     return fcg
+
+def get_method_label(method):
+    pointer_index = method.find('->')
+    bracket_index = method.find('(', pointer_index)
+    class_name = 'L' + method[0:pointer_index]
+    method_name = method[pointer_index+2:bracket_index]
+    parameter = method[bracket_index:]
+    return (class_name, method_name, parameter)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -53,7 +64,7 @@ def main():
                     cg, graphdir = generate(os.path.join(parent, filename))
                     fcg = networkxify(cg)
                     h = os.path.splitext(filename)[0]
-                    fnx = os.path.join(out, "{}.pz".format(h))
+                    fnx = os.path.join(graphdir, "{}.pz".format(h))
                     pz.save(fcg, os.path.join(graphdir, fnx))
 
                     # progressbar
