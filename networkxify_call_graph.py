1eab972ed5bd8e9116578f5816566346e2dd5d83
Chushu <liuchushu10@gmail.com>
2017-03-10 17:13:51 +0800

    presently save work

--- networkxify_call_graph.py
+++ networkxify_call_graph.py
@@ -6,7 +6,34 @@ import os, argparse
 from common.utils import use_progressbar, count_file
 
 def networkxify(cg):
-    pass
+    """ Using NX and Smali, build a directed graph NX object so that:
+        - node names are method names as: class name, method name and
+          descriptor
+        - each node has a label that encodes the method behavior
+    """
+    # nx graph for FCG extracted from APK: nodes = method_name,
+    # labels = encoded instructions
+    fcg = nx.DiGraph()
+    methods = self.d.get_methods()
+    for method in methods:
+        node_name = self.get_method_label(method)
+
+        # find calls from this method
+        children = []
+        for cob in method.XREFto.items:
+            remote_method = cob[0]
+            children.append(self.get_method_label(remote_method))
+
+        # find all instructions in method and encode using coloring
+        instructions = []
+        for i in method.get_instructions():
+            instructions.append(i.get_name())
+        encoded_label = self.color_instructions(instructions)
+        # add node, children and label to nx graph
+        fcg.add_node(node_name, label=encoded_label)
+        fcg.add_edges_from([(node_name, child) for child in children])
+
+    return fcg
 
 def main():
     parser = argparse.ArgumentParser()
@@ -24,7 +51,10 @@ def main():
                 if filename.endswith('.apk'):
                     # print(os.path.join(parent, filename))
                     cg, graphdir = generate(os.path.join(parent, filename))
-
+                    fcg = networkxify(cg)
+                    h = os.path.splitext(filename)[0]
+                    fnx = os.path.join(out, "{}.pz".format(h))
+                    pz.save(fcg, os.path.join(graphdir, fnx))
 
                     # progressbar
                     progress += 1
